0de2aadcfda739f7ef6d68e5602db918b4e8cd77
Sundae <dinoy1976ronnie@gmail.com>
2023-04-16 13:04:55 +0800

    First commits

---
+++ main.js
@@ -0,0 +1,6 @@
+let inputName = document.getElementById("name").value; 
+let inputPrice = document.getElementById("price").value; 
+let inputQuantity = document.getElementById("quantity").value;
+let addFish = document.getElementById("ADD").value;
+
+prompt(inputName);
